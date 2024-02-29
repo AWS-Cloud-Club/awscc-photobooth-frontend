907a74573d4559f55db9e9534b05570845ee9c37
Rafael Louie Miguel <118577897+egg-lou@users.noreply.github.com>
2024-02-29 13:34:56 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const PORT = 80;
+const PORT = 3000;
 const app = express();
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
